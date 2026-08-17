b0cd77e0cc367551baac1cf4608dfd3b6cfbd354
Faiq Subhi Ramadlan <faiqsr13@gmail.com>
2026-08-17 17:57:48 +0700

    feat: add reusable primary button blade component

--- resources/views/components/btn-primary.blade.php
+++ resources/views/components/btn-primary.blade.php
@@ -5,7 +5,4 @@
            px-8 py-3 rounded-lg font-medium text-base transition-all duration-300
            shadow-md hover:shadow-lg hover:-translate-y-0.5 {{ $class }}">
     <span>{!! $label !!}</span>
-    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"/>
-    </svg>
 </a>
